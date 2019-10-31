96bea5d3eb77df8e9aae702a2444ea5f531ae9ca
Guilherme Chaves <48635609+GuilhermehChaves@users.noreply.github.com>
2019-10-31 19:13:31 -0300

    Add files via upload

--- youtube.py
+++ youtube.py
@@ -1,9 +1,17 @@
 from __future__ import unicode_literals
 import youtube_dl
 import os
+from video import Video
 
 ydl_options = {}
 os.chdir('./videos')
 
 with youtube_dl.YoutubeDL(ydl_options) as ydl:
-  ydl.download(['https://www.youtube.com/watch?v=GZgnl5Ocuh8'])
+  info = ydl.extract_info('https://www.youtube.com/watch?v=tVWF0hcbx58&t=88s')
+  # ydl.download(['https://www.youtube.com/watch?v=tVWF0hcbx58&t=88s'])
+
+
+os.chdir('../') 
+video = Video('saitama', './videos/'+info['title'] + '-' + info['id'] + '.mp4')
+video.cut()
+# print(info['title'],info['id'])
