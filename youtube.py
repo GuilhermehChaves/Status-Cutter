66c74eaffedb8bff21aa712f9e31a98d396c0d1a
Guilherme Chaves <48635609+GuilhermehChaves@users.noreply.github.com>
2019-11-01 11:08:35 -0300

    Add files via upload

--- youtube.py
+++ youtube.py
@@ -4,14 +4,17 @@ import os
 from video import Video
 
 ydl_options = {}
-os.chdir('./videos')
+os.chdir('./videos')#trocando de diretorio para salvar no diretorio ./videos
 
 with youtube_dl.YoutubeDL(ydl_options) as ydl:
-  info = ydl.extract_info('https://www.youtube.com/watch?v=tVWF0hcbx58&t=88s')
+  info = ydl.extract_info('https://www.youtube.com/watch?v=WNeLUngb-Xg')
   # ydl.download(['https://www.youtube.com/watch?v=tVWF0hcbx58&t=88s'])
 
 
-os.chdir('../') 
-video = Video('saitama', './videos/'+info['title'] + '-' + info['id'] + '.mp4')
+os.chdir('../') #voltando para o diretório raiz
+
+os.rename('./videos/'+info['title'] + '-' + info['id'] + '.mp4', './videos/In the end.mp4')
+
+video = Video('In the end', './videos/In the end.mp4')
 video.cut()
 # print(info['title'],info['id'])
